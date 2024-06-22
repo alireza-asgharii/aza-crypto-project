f0e2eea8269ca7fe7ee44e8596e4d511925e204a
Alireza Asghari <aa9967316@gmail.com>
2024-06-22 12:48:48 +0330

    🧑‍💻 Use variable for api key

--- src/services/queries.js
+++ src/services/queries.js
@@ -1,25 +1,27 @@
 import axios from "axios";
 
+const apiKey = import.meta.env.VITE_API_KEY
+
 export const fetchCoinsMarket = (page = 1) => {
   return axios.get(
     `/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${page}&sparkline=true&price_change_percentage=1h%2C24%2C7d&x_cg_demo_api_key=${
-      import.meta.env.VITE_API_KEY
+      apiKey
     }`
   );
 };
 
 export const fetchTrandData = () => {
   return axios.get(
-    `/search/trending?x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`
+    `/search/trending?x_cg_demo_api_key=${apiKey}`
   );
 };
 
 export const fetchSearch = (value) => {
   return axios.get(
-    `/search?query=${value}&x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`
+    `/search?query=${value}&x_cg_demo_api_key=${apiKey}`
   );
 };
 
 export const fetchGlobalData = () => {
-  return axios.get(`/global?x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`);
+  return axios.get(`/global?x_cg_demo_api_key=${apiKey}`);
 };
