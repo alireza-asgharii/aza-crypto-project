bf8dbac28856caaef70498983152178c9b3697a1
Alireza Asghari <aa9967316@gmail.com>
2024-06-22 12:47:59 +0330

    🔧 Change api base URL

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_USRL;
+axios.defaults.baseURL = import.meta.env.VITE_BASE_USRL_MAIN;
 
 axios.interceptors.response.use((response) => {
   return response.data;
