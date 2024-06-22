16ee1eb7e6046de8fa5c1c53efcb44960ebd5f8c
Alireza Asghari <aa9967316@gmail.com>
2024-06-22 12:13:26 +0330

    ⚡️ Use ReactQuery for fetch data

--- src/hooks/useQueries.jsx
+++ src/hooks/useQueries.jsx
@@ -1,5 +1,5 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { fetchCoinsMarket } from "../services/queries";
+import { fetchCoinsMarket, fetchGlobalData, fetchSearch, fetchTrandData } from "../services/queries";
 
 export const useGetCoinsMarket = (page) => {
   const res = useQuery({
@@ -7,6 +7,47 @@ export const useGetCoinsMarket = (page) => {
     queryFn: () => fetchCoinsMarket(page),
     placeholderData: keepPreviousData,
     refetchInterval: 1 * 60 * 1000,
+  });
+
+  return res;
+};
+
+export const useGet = (page) => {
+  const res = useQuery({
+    queryKey: ["coinsMarket", page || 1],
+    queryFn: () => fetchCoinsMarket(page),
+    placeholderData: keepPreviousData,
+    refetchInterval: 1 * 60 * 1000,
+  });
+
+  return res;
+};
+
+export const useGetTrandData = () => {
+  const res = useQuery({
+    queryKey: ["trandData"],
+    queryFn: fetchTrandData,
+    placeholderData: keepPreviousData,
+  });
+
+  return res;
+};
+
+export const useGetSearch = (value = "btc") => {
+  const res = useQuery({
+    queryKey: ["searchData", value],
+    queryFn: () => fetchSearch(value),
+    placeholderData: keepPreviousData,
+  });
+
+  return res;
+};
+
+export const useGetGlobalData = () => {
+  const res = useQuery({
+    queryKey: ["globalData"],
+    queryFn: fetchGlobalData,
+    placeholderData: keepPreviousData,
   });
 
   return res;
--- src/services/queries.js
+++ src/services/queries.js
@@ -1,5 +1,25 @@
-import axios from "axios"
+import axios from "axios";
 
-export const fetchCoinsMarket  = (page=1) => {
-  return axios.get(`${import.meta.env.VITE_BASE_USRL_MAIN}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${page}&sparkline=true&price_change_percentage=1h%2C24%2C7d&x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`)
-}
+export const fetchCoinsMarket = (page = 1) => {
+  return axios.get(
+    `/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${page}&sparkline=true&price_change_percentage=1h%2C24%2C7d&x_cg_demo_api_key=${
+      import.meta.env.VITE_API_KEY
+    }`
+  );
+};
+
+export const fetchTrandData = () => {
+  return axios.get(
+    `/search/trending?x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`
+  );
+};
+
+export const fetchSearch = (value) => {
+  return axios.get(
+    `/search?query=${value}&x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`
+  );
+};
+
+export const fetchGlobalData = () => {
+  return axios.get(`/global?x_cg_demo_api_key=${import.meta.env.VITE_API_KEY}`);
+};
