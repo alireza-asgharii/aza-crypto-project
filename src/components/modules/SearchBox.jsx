f5f8eee5c17a2df0c4e1b4878458ecba685eacfc
Alireza Asghari <aa9967316@gmail.com>
2024-06-24 17:00:54 +0330

    🐛 Fix Search box bug

--- src/components/modules/SearchBox.jsx
+++ src/components/modules/SearchBox.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "../../styles/navbar.module.scss";
 
 import { MdLocalFireDepartment } from "react-icons/md";
@@ -17,15 +17,24 @@ const SearchBox = ({
   searchIsError,
   searchData,
   setMobileMenu,
-  mobileMenu
+  mobileMenu,
 }) => {
-  const inputRef = useRef(null)
+  const inputRef = useRef(null);
+  const [windowSize, setWindowSize] = useState(() => window.innerWidth);
+
+  window.addEventListener("resize", (e) => {
+    setWindowSize(e.currentTarget.innerWidth);
+  });
 
   useEffect(() => {
-    if (mobileMenu) inputRef.current.focus()
-  }, [mobileMenu])
+    if (mobileMenu) inputRef.current.focus();
+  }, [mobileMenu]);
   return (
-    <label className={`${styles.searchLabel} rounded-md ${isShow ? "rounded-b-none" : ""}`}>
+    <label
+      className={`${styles.searchLabel} rounded-md ${
+        isShow ? "rounded-b-none" : ""
+      }`}
+    >
       <div className="flex justify-between items-center w-full py-2">
         <div className="flex items-center rounded-md overflow-hidden">
           <FiSearch className="mr-1 opacity-80 font-bold text-sm pt-[1px]" />
@@ -35,12 +44,22 @@ const SearchBox = ({
             placeholder="Search"
             onChange={changeHandler}
             onFocus={() => setIsShow(true)}
-            onBlur={() => setTimeout(() => setIsShow(false), 200)}
+            onBlur={() =>
+              windowSize > 760 ? setTimeout(() => setIsShow(false), 200) : ""
+            }
             ref={inputRef}
           />
         </div>
         <div className="flex justify-center items-center md:hidden">
-          <button className="rounded-md bg-[#5463FF] px-3 py-[6px] mt-[5px] text-xs cursor-default" onClick={() => setMobileMenu(prev => !prev)}>Cancel</button>
+          <button
+            className="rounded-md bg-[#5463FF] px-3 py-[6px] mt-[5px] text-xs cursor-default"
+            onClick={() => {
+              setMobileMenu((prev) => !prev);
+              setIsShow((prev) => !prev);
+            }}
+          >
+            Cancel
+          </button>
         </div>
       </div>
 
@@ -50,13 +69,21 @@ const SearchBox = ({
         data.length !== 0 &&
         typeof data === "object" &&
         !isError && (
-          <div className={`absolute z-[101] left-0 top-[50px] md:top-[36px]  w-full bg-[#171924] text-white rounded-md p-[25px_0px_15px] block max-h-[400px] overflow-y-auto ${isShow ? "rounded-t-none" : ""}`}>
+          <div
+            className={`absolute z-[101] left-0 top-[50px] md:top-[36px]  w-full bg-[#171924] text-white rounded-md p-[25px_0px_15px] block max-h-[400px] overflow-y-auto ${
+              isShow ? "rounded-t-none" : ""
+            }`}
+          >
             <div className="flex items-center px-3">
               <h6 className="text-[#5e6172] font-medium text-xs">Tranding</h6>
               <MdLocalFireDepartment className="text-[#ff775f] ml-1 text-xl pt-1 " />
             </div>
             {data.coins.map((coin) => (
-              <SearchItem key={coin.item.id} data={coin.item} setMobileMenu={setMobileMenu} />
+              <SearchItem
+                key={coin.item.id}
+                data={coin.item}
+                setMobileMenu={setMobileMenu}
+              />
             ))}
           </div>
         )}
@@ -66,7 +93,13 @@ const SearchBox = ({
         : isShow &&
           inputValue !== "" &&
           searchData && (
-            <div className={`${styles.searchItemContainer} top-[50px] md:top-[36px] rounded-md  ${isShow ? "rounded-t-none" : ""}`}>
+            <div
+              className={`${
+                styles.searchItemContainer
+              } top-[50px] md:top-[36px] rounded-md  ${
+                isShow ? "rounded-t-none" : ""
+              }`}
+            >
               <div className={styles.trandigContainer}>
                 <h6 className="text-[#5e6172] font-medium text-xs px-2 pb-2">
                   Cryptoassets
